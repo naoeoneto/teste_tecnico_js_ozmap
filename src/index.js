3a1b8b1a22f7c600bb12002c3d9a4400632ca4d1
naoeoneto <abdpneto@gmail.com>
2023-06-08 10:21:06 -0300

    add develop

--- src/index.js
+++ src/index.js
@@ -7,28 +7,28 @@
 // todas as configuraçoes devem ser passadas via environment variables
 const PORT = process.env.PORT || 3000;
 
-const Koa = require('koa');
-const Router = require('koa-router');
+const Koa = require("koa");
+const Router = require("koa-router");
 
 const koa = new Koa();
 var router = new Router();
 
 //rota simples pra testar se o servidor está online
-router.get('/', async (ctx) => {
+router.get("/", async (ctx) => {
   ctx.body = `Seu servidor esta rodando em http://localhost:${PORT}`; //http://localhost:3000/
 });
 
 //Uma rota de exemplo simples aqui.
 //As rotas devem ficar em arquivos separados, /src/controllers/userController.js por exemplo
-router.get('/users', async (ctx) => {
-    ctx.status = 200;
-    ctx.body = {total:0, count: 0, rows:[]}
+router.get("/users", async (ctx) => {
+  ctx.status = 200;
+  ctx.body = { total: 0, count: 0, rows: [] };
 });
 
-koa
-  .use(router.routes())
-  .use(router.allowedMethods());
+koa.use(router.routes()).use(router.allowedMethods());
 
-const server = koa.listen(PORT);
+const server = koa.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = server;
