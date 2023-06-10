93bc15e509a000214a3f0e2a9066f96af9a4b3e7
naoeoneto <abdpneto@gmail.com>
2023-06-10 18:54:19 -0300

    feat: initial doc commit

--- src/index.js
+++ src/index.js
@@ -12,24 +12,44 @@ const bodyparser = require("koa-bodyparser");
 const Router = require("koa-router");
 const userRouter = require("./routes/user.routes");
 const errorHandler = require("../src/errors/handleError");
+// const swagger = require("koa2-swagger-ui");
+const yamljs = require("yamljs");
+const swagger = require("swagger2");
+const koaSwagger = require("swagger2-koa");
 
+// const docs = new Router({ prefix: "/" });
 const koa = new Koa();
 var router = new Router();
+// const spec = yamljs.load("api.yml");
+const swaggerDocument = swagger.loadDocumentSync("./swagger.yml");
+// var swagger = require("swagger-koa");
+
+// koa.use(
+//   swagger.init({
+//     apiVersion: "1.0",
+//     swaggerVersion: "1.0",
+//     swaggerURL: "/swagger",
+//     swaggerJSON: "/swagger.json",
+//     swaggerUI: "./public/swagger/",
+//     basePath: "http://localhost:3000",
+//     info: {
+//       title: "swagger-koa sample app",
+//       description: "Swagger + Koa = {swagger-koa}",
+//     },
+//     apis: ["./api.yml"],
+//   })
+// );
 
 //rota simples pra testar se o servidor está online
 router.get("/", async (ctx) => {
   ctx.body = `Seu servidor esta rodando em http://localhost:${PORT}`; //http://localhost:3000/
 });
-// koa.use(async (ctx, next) => {
-//   try {
-//     await next();
-//   } catch (err) {
-//     err.status = err.statusCode || err.status || 500;
-//     ctx.body = err.message;
-//     console.log("oi", ctx.body);
-//     ctx.app.emit("error", err, ctx);
-//   }
-// });
+
+// docs.get(
+//   "/swagger",
+//   swagger.koaSwagger({ routePrefix: false, swaggerOptions: { spec } })
+// );
+
 koa.use(async (ctx, next) => {
   try {
     await next();
@@ -44,10 +64,18 @@ koa.use(async (ctx, next) => {
 
 koa.use(errorHandler);
 koa.on("error", (err, ctx) => {
-  console.log("oi", err);
+  console.log(err);
 });
+if (!swagger.validateDocument(swaggerDocument)) {
+  throw Error(`./swagger.yml does not conform to the Swagger 2.0 schema`);
+}
+
+koa
+  // .use(koaSwagger.router(swaggerDocument, "/swagger"))
+  .use(koaSwagger.validate(swaggerDocument));
 
 koa.use(bodyparser());
+// koa.use(docs.routes()).use(docs.allowedMethods());
 koa.use(router.routes()).use(router.allowedMethods());
 koa.use(userRouter.routes()).use(userRouter.allowedMethods());
 
